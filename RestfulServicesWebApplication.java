374ed4e3b6e9f19abb424301c57d5edb05f28f64
Nilesh Singh <65400085+Nilesh9984@users.noreply.github.com>
2021-06-24 02:15:32 +0530

    Add files via upload

---
+++ RestfulServicesWebApplication.java
@@ -0,0 +1,13 @@
+package com.restfulwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulServicesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulServicesWebApplication.class, args);
+	}
+
+}
